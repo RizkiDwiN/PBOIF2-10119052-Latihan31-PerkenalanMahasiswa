33c268e3286de89f4b10a7f34d3b1f369419ffa2
Kiki <Kiki@Kiki-PC>
2020-11-05 20:20:29 +0700

    Upload Project

---
+++ src/pboif2/pkg10119052/latihan31/perkenalanmahasiswa/Mahasiswa.java
@@ -0,0 +1,38 @@
+package pboif2.pkg10119052.latihan31.perkenalanmahasiswa;
+
+/**
+ *
+ * NAMA   : Rizki Dwi nugraha
+ * KELAS  : IF-2
+ * NIM    : 10119052
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * class perkenalanDiri void
+ * 
+ */
+public class Mahasiswa {
+    
+    public static void main(String[] args) {
+        PBOIF210119052Latihan31PerkenalanMahasiswa mhs = new PBOIF210119052Latihan31PerkenalanMahasiswa();
+        
+        mhs.nim = "10119052";
+        mhs.nama = "Rizki Dwi nugraha";
+        
+        mhs.perkenalanDiri();
+        
+        mhs.nim = "10110270";
+        mhs.nama = "Indra Tiola";
+        
+        mhs.perkenalanDiri();
+        
+        mhs.nim = "10110271";
+        mhs.nama = "Robi Tanzil Ganafi";
+        
+        mhs.perkenalanDiri();
+        
+        mhs.nim = "10110269";
+        mhs.nama = "Muhammad Nur Awaludin";
+        
+        mhs.perkenalanDiri();
+    }
+}
+
---
+++ src/pboif2/pkg10119052/latihan31/perkenalanmahasiswa/PBOIF210119052Latihan31PerkenalanMahasiswa.java
@@ -0,0 +1,24 @@
+package pboif2.pkg10119052.latihan31.perkenalanmahasiswa;
+
+/**
+ *
+ * NAMA   : Rizki Dwi nugraha
+ * KELAS  : IF-2
+ * NIM    : 10119052
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * class perkenalanDiri void
+ * 
+ */
+public class PBOIF210119052Latihan31PerkenalanMahasiswa {
+
+        public  String nama;
+        public  String nim;
+        
+    public void perkenalanDiri() {
+        System.out.println("Hallo Everyone");
+        System.out.println("My NIM " + nim);
+        System.out.println("My Name is " + nama);
+        System.out.println("");
+    }
+    
+}
